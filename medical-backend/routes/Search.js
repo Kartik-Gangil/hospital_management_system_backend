a559c8e67f427bd6175fec5b1261af7909876d79
Kartik-Gangil <kartikgangil@gmail.com>
2026-02-26 18:41:22 +0000

    fixing the search route to correctly import prisma client

--- medical-backend/routes/Search.js
+++ medical-backend/routes/Search.js
@@ -2,9 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+const prisma = require("../controller/DB");
 
 /**
  * GET /api/sale-bills/search
